565b7e77cc20afd190bb9267a450df3defb5c442
Brad <bradharris@harrismbrad.com>
2022-12-13 06:30:50 -0800

    added minify, and prod set state

---
+++ production.js
@@ -0,0 +1,23 @@
+// Production after QA Passed 
+
+function LikeButton() {
+  const [liked, setLiked] = React.useState(false);
+
+  if (liked) {
+    return 'You liked this!';
+  }
+
+  return React.createElement(
+    'button',
+    {
+      onClick: () => setLiked(true),
+    },
+    'Like'
+  );
+}
+
+const rootNode = document.getElementById('like-button-root');
+const root = ReactDOM.createRoot(rootNode);
+root.render(React.createElement(LikeButton));
+
+
